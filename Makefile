b399daac26d344b18ba13aff0431a2af10994d33
Pawel Koperek <p.koperek@eyeo.com>
2018-09-24 09:55:12 +0200

    bump up the docker version finalize model runner script

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.5
+VERSION=0.6
 
 release:
 	docker build -t pkoperek/dqn-manager:latest -t pkoperek/dqn-manager:${VERSION} .
--- test_run.py
+++ test_run.py
@@ -9,7 +9,6 @@ from itertools import count
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torch.nn.functional as F
 
 env = gym.make('SingleDCAppEnv-v0').unwrapped
@@ -23,14 +22,11 @@ Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
 
 
-storage_path = os.getenv('STORAGE_PATH', '/storage')
-
-
 class DQN(nn.Module):
 
     def __init__(self):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv1d(5, 16, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv1d(6, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm1d(16)
         self.conv2 = nn.Conv1d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm1d(32)
@@ -39,7 +35,6 @@ class DQN(nn.Module):
         self.head = nn.Linear(32 * 222, 3)
 
     def forward(self, x):
-        print("Network input: " + str(x.size()))
         x = F.selu(self.bn1(self.conv1(x)))
         x = F.selu(self.bn2(self.conv2(x)))
         x = F.selu(self.bn3(self.conv3(x)))
@@ -54,36 +49,20 @@ def get_measurements():
     return measurements
 
 
-env.reset()
-
-BATCH_SIZE = 128
-GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
-TARGET_UPDATE = 10
-
-policy_net = DQN().to(device)
-target_net = DQN().to(device)
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
-
-optimizer = optim.RMSprop(policy_net.parameters())
-
-steps_done = 0
-
-policy_net = torch.load(storage_path + '/policy_net_full.dump')
-target_net = torch.load(storage_path + '/target_net_full.dump')
-
-
 def select_action(state):
     with torch.no_grad():
         return policy_net(state).max(1)[1].view(1, 1)
 
 
-episode_durations = []
+storage_path = os.getenv('STORAGE_PATH', '/storage')
+loaded_state_dict = torch.load(storage_path + '/policy_net_state.dump')
+policy_net = DQN().to(device)
+policy_net.load_state_dict(loaded_state_dict)
+
+env.reset()
 
 total_reward = 0
+total_wait = 0
 # Initialize the environment and state - restart the simulation
 env.reset()
 last_measurements = get_measurements()
@@ -95,33 +74,28 @@ for t in count():
     print("Iteration: " + str(t))
     # Select and perform an action
     action = select_action(state)
-    _, reward, done, _ = env.step(action.item())
+    obs, reward, done, _ = env.step(action.item())
     total_reward += reward
-    print("Reward for action: " + str(reward) + " act: " + str(action))
-    reward = torch.tensor([reward], device=device)
+    total_wait += obs[5]
+    print("Reward for action: " + str(reward) +
+          " act: " + str(action) +
+          " total reward: " + str(total_reward) +
+          " total wait: " + str(total_wait) +
+          " obs: " + str(list(obs)))
 
     # Observe new state
     last_measurements = current_measurements
     current_measurements = get_measurements()
 
-    number_of_vms = current_measurements[0][0][-1].item()
-    a = action[0][0].item()
-
-    print("LOG it vm act: {} {} {}".format(
-        str(t),
-        str(number_of_vms),
-        str(a),
-    ))
-
     if not done:
         next_state = current_measurements - last_measurements
     else:
-        next_state = None
+        break
 
     # Move to the next state
     state = next_state
 
-    print("Total reward: " + str(total_reward))
-
 print('Complete')
+print("Total reward: " + str(total_reward) + " total wait time: " + str(total_wait))
+
 env.close()
