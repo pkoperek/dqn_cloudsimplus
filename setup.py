f866c6ea446e27b3c9689ac4a7047069a65294cc
Pawel Koperek <pkoperek@gmail.com>
2018-12-13 14:02:57 +0100

    update setup.py with psycopg2-binary

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup
 setup(
     name='dqn_cloudsimplus',
     version='0.0.1',
-    install_requires=['gym', 'gym_cloudsimplus', 'torch', 'torchvision']
+    install_requires=[
+        'gym',
+        'gym_cloudsimplus',
+        'torch',
+        'torchvision',
+        'psycopg2-binary',
+    ]
 )
